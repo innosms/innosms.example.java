5470808cd671f4feaf1e0f42e256b7edf4b51966
innosms <tech@innopost.com>
2014-12-01 10:13:11 +0900

    java example add

--- src/Config.java
+++ src/Config.java
@@ -1,6 +1,6 @@
 
 
 public final class Config {
-	public static final String CLIENT_ID = "M100001";
-	public static final String API_KEY = "89FA695592784613CCE96C09555CD719";
+	public static final String CLIENT_ID = "발급받은 API 아이디";
+	public static final String API_KEY = "발급받은 API키";
 }
