257ff05e351372af24b2e0b964f2737a4c90eea4
innosms <tech@innopost.com>
2014-12-04 17:25:20 +0900

    java example

--- src/GetBalance.java
+++ src/GetBalance.java
@@ -4,16 +4,18 @@ import com.innosms.api.*;
 public class GetBalance {
 
 	public static void main(String[] args) {
-		
-		MessagingService messagingService = new MessagingService(Config.CLIENT_ID,Config.API_KEY);
-		String money = messagingService.getBalance();
-		
-		/******************************************************************************
-		- 결과값
-		money : 잔여금액
-		******************************************************************************/
-		System.out.println(money);
-		
+		try{
+			MessagingService messagingService = new MessagingService(Config.CLIENT_ID,Config.API_KEY);
+			String money = messagingService.getBalance();
+			
+			/******************************************************************************
+			- 결과값
+			money : 잔여금액
+			******************************************************************************/
+			System.out.println(money);
+		}catch(Exception e){			
+			System.out.println(e);
+		}
 	}
 
 }
