63c5c60c229e4dd50bded63401146d0a8281197c
INNOPOST <tech@innopost.com>
2017-07-25 10:38:53 +0900

    update
    update

--- src/com/innosms/api/MsgContent.java
+++ src/com/innosms/api/MsgContent.java
@@ -11,60 +11,60 @@ public class MsgContent{
 	String msg;
 	JSONObject msgList;
 	String image;
-	
+
 	public String getMsgType() {
 		return msgType;
 	}
 	public void setMsgType(String msg_type) {
 		this.msgType = msg_type;
 	}
-	
+
 	public String getPhone() {
 		return phone;
 	}
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
-	
+
 	public String getCallback() {
 		return callback;
 	}
 	public void setCallback(String callback) {
 		this.callback = callback;
 	}
-	
+
 	public String getTrandate() {
 		return trandate;
 	}
 	public void setTrandate(String trandate) {
 		this.trandate = trandate;
 	}
-	
+
 	public String getSubject() {
 		return subject;
 	}
 	public void setSubject(String subject) {
 		this.subject = subject;
 	}
-	
+
 	public String getMsg() {
 		return msg;
 	}
 	public void setMsg(String msg) {
 		this.msg = msg;
 	}
-	
+
 	public JSONObject getMsgList() {
 		return msgList;
 	}
 	public void setMsgList(JSONObject msgList) {
 		this.msgList = msgList;
 	}
-	
+
 	public String getImage() {
 		return image;
 	}
 	public void setImage(String image) {
 		this.image = image;
-	}	
+	}
 }
